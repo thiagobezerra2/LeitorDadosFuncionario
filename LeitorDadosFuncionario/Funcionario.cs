db2d321cf0bed65811eef8518b895370d3fa9426
016169631 <016169631@IBM-PE08SYNC>
2022-09-23 00:59:55 -0300

    Adicionar arquivos de projeto.

---
+++ LeitorDadosFuncionario/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace LeitorDadosFuncionario {
+    class Funcionario {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+        public double SalarioLiq() {
+            return Salario - Imposto;
+        }
+        public void Aumento(double porcentagem) {
+            Salario = Salario + (Salario * porcentagem / 100); 
+        }
+        public override string ToString() {
+            return Nome + ", R$ " + SalarioLiq().ToString("F2", CultureInfo.InvariantCulture);
+               ;
+        }
+    }
+}
---
+++ LeitorDadosFuncionario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace LeitorDadosFuncionario {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario func = new Funcionario();
+            Console.Write("Nome:");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: "  + func.Nome + ", $" + func.SalarioLiq().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine("Digite a porcentagem para aumentar o salário:" );
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.Aumento(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
